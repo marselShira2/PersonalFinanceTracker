7e57f3efa702fed3cfc44618ffb74e3f77220f8c
Klea Vataj <kleavataj56@gmail.com>
2025-12-31 17:33:15 +0100

    FIx

---
+++ FinanceTracker.Server/Interfaces/INotificationRepository.cs
@@ -0,0 +1,13 @@
+﻿using FinanceTracker.Server.Models;
+
+namespace FinanceTracker.Server.Interfaces
+{
+    public interface INotificationRepository
+    {
+        Task<List<Notification>> GetNotificationsAsync(int userId);
+
+        Task MarkAsReadAsync(int notificationId);
+
+        Task<int> GetUnreadCountAsync(int userId);
+    }
+}
